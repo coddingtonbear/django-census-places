a4203d376a4ab4934cb38637c1b3192f6def6e7e
Adam Coddington <me@adamcoddington.net>
2012-07-15 18:42:27 -0700

    Adding columns to admin display.

--- census_places/admin.py
+++ census_places/admin.py
@@ -3,8 +3,19 @@ from django.contrib.gis import admin
 from census_places.models import PlaceBoundary
 
 class PlaceBoundaryAdmin(admin.options.OSMGeoAdmin):
-    search_fields = (
-                'name', 
+    list_display = (
+            'name',
+            'get_state_display',
+            'place',
+            'lsad',
+            'censusarea',
             )
+    search_fields = (
+                'name',
+            )
+
+    def get_state_display(self, obj):
+        return obj.get_state_display()
+    get_state_display.short_description = 'State'
 
 admin.site.register(PlaceBoundary, PlaceBoundaryAdmin)
